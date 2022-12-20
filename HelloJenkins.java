2745454090e4a475bab2a63e7a06991240d2313c
suramya-biswas-git <suramya.biswas@gmail.com>
2022-12-20 12:52:47 +0530

    Hello Jenkins Second Version

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,6 @@
 public class HelloJenkins{
   public static void main(String [] arg){
      System.out.println("Welcome To Java & Jenkins setup");
+     System.out.println("Welcome To Git & Jenkins setup");
       }
 }
